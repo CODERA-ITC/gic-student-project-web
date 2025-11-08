ae2f9bd2c5c667ad727d8cddae83373a19b938f7
Yem Daro <daroyem99@gmail.com>
2025-11-08 19:30:21 +0700

    format: use eslint rules from the nuxt team

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@vueuse/nuxt"],
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@vueuse/nuxt", "@nuxt/eslint"],
 
   // Vite configuration
   vite: {
