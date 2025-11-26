2085101c16dbd3e300bcc15ea90d6d5f25344166
Yem Daro <daroyem99@gmail.com>
2025-11-26 23:23:26 +0700

    add TechInfo component

--- app/components/app/Hero.vue
+++ app/components/app/Hero.vue
@@ -1,10 +1,9 @@
 <script setup>
-import { motion, MotionValue, useAnimationControls, useAnimationFrame, useMotionValue, useTime, useTransform } from "motion-v";
-import { stop } from "vue";
+import { motion, useAnimationFrame, useMotionValue, useTime, useTransform } from 'motion-v'
 
-const duration = 5; // 2 sec
-const distance = 180;
-const origin = 20;
+const duration = 5 // 2 sec
+const distance = 180
+const origin = 20
 
 const isPaused = ref(false)
 const time = useTime()
@@ -15,7 +14,8 @@ const pauseOffset = useMotionValue(0)
 useAnimationFrame(() => {
   if (isPaused.value) {
     pauseOffset.set(time.get() - pausedTime.get())
-  } else {
+  }
+  else {
     pausedTime.set(time.get() - pauseOffset.get())
   }
 })
@@ -25,198 +25,268 @@ function togglePause() {
 }
 
 const count = 7
-const multiplier = Array.from({ length: count }, (_, i) => i * 850);
+const multiplier = Array.from({ length: count }, (_, i) => i * 850)
 const pos = Array.from({ length: count }, (_, i) => {
   const timeOffset = multiplier[i] * duration
-  
+
   return {
-    x: useTransform(() => 
-      Math.cos((pausedTime.get() + timeOffset) / 1000 / duration) * distance + origin
+    x: useTransform(() =>
+      Math.cos((pausedTime.get() + timeOffset) / 1000 / duration) * distance + origin,
     ),
-    y: useTransform(() => 
-      Math.sin((pausedTime.get() + timeOffset) / 1000 / duration) * distance + origin
+    y: useTransform(() =>
+      Math.sin((pausedTime.get() + timeOffset) / 1000 / duration) * distance + origin,
     ),
   }
 })
 
+const techInfoShouldShow = ref([true, false])
+function handleTechMouseEnter(index) {
+  // togglePause()
+  techInfoShouldShow.value[index] = true
+  isPaused.value = true
+}
+function handleTechMouseLeave(index) {
+  // togglePause()
+  techInfoShouldShow.value[index] = false
+  isPaused.value = false
+}
+const techInfoData = ref(
+  [
+    {
+      title: 'Javascript',
+      content: 'Web Development',
+    },
+    {
+      title: 'VueJs',
+      content: 'Frontend Development',
+    },
+    {
+      title: 'o ro cute',
+      content: 'Frontend Development',
+    },
+  ],
+)
 </script>
 
 <template>
   <section
+    id="hero"
     class="hero-section justify-center md:items-center lg:items-start md:justify-center lg:justify-around flex flex-row md:flex-col lg:flex-row"
   >
-  
     <div class="relative top-20 w-40 right-20 h-max">
       <div class="relative top-28 left-12">
-        <img src="/images/tech/blackhole.png" width="120" alt="" />
+        <img src="/images/tech/blackhole.png" width="120" alt="">
       </div>
 
-      <motion.div class="relative left-10" 
-      @mouseenter="togglePause()" @mouseleave="togglePause()" 
-      :style="pos[0]"
+      <motion.div
+        class="relative left-10"
+        :style="pos[0]"
       >
-        <img
-          class="absolute"
-          src="/images/tech/javascript-logo.jpg"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-15"
-          src="/images/tech/vue-logo.png"
-          width="60"
-          alt=""
-        />
+        <div
+          class="relative"
+          @mouseenter="handleTechMouseEnter(0)"
+          @mouseleave="handleTechMouseLeave(0)"
+        >
+          <AppTechInfo
+            v-if="techInfoShouldShow[0]"
+            class="absolute z-100 t"
+            :tech-info="techInfoData[2]"
+          />
+          <img
+            class="absolute"
+            src="/images/tech/javascript-logo.jpg"
+            width="60"
+            alt=""
+          >
+        </div>
+
+        <div
+          class="relative"
+          @mouseenter="handleTechMouseEnter(1)"
+          @mouseleave="handleTechMouseLeave(1)"
+        >
+          <AppTechInfo
+            v-if="techInfoShouldShow[1]"
+            class="absolute left-20 top-10 z-100"
+            :tech-info="techInfoData[1]"
+          />
+          <img
+            class="absolute left-15"
+            src="/images/tech/vue-logo.png"
+            width="60"
+            alt=""
+          >
+        </div>
         <img
           class="absolute left-10 top-10"
           src="/images/tech/react-logo.png"
           width="60"
           alt=""
-        />
+        >
       </motion.div>
 
-      <motion.div class="relative left-8"
-      @mouseenter="togglePause()" @mouseleave="togglePause()" 
-      :style="pos[1]">
+      <motion.div
+        class="relative left-8"
+        :style="pos[1]"
+      >
         <img
           class="absolute"
           src="/images/tech/python-logo.png"
           width="60"
           alt=""
-        />
+          @mouseenter="handleTechMouseEnter(3)"
+          @mouseleave="handleTechMouseLeave(3)"
+        >
         <img
           class="absolute left-15"
           src="/images/tech/tensorflow-logo.png"
           width="60"
           alt=""
-        />
+          @mouseenter="handleTechMouseEnter(3)"
+          @mouseleave="togglePause(3)"
+        >
         <img
           class="absolute left-10 top-10"
           src="/images/tech/torch-logo.png"
           width="60"
           alt=""
-        />
+          @mouseenter="handleTechMouseEnter(3)"
+          @mouseleave="togglePause(3)"
+        >
       </motion.div>
 
-      <motion.div class="relative left-8"
-      @mouseenter="togglePause()" @mouseleave="togglePause()"
-      :style="pos[2]">
+      <motion.div
+        class="relative left-8"
+        :style="pos[2]"
+        @mouseenter="togglePause()"
+        @mouseleave="togglePause()"
+      >
         <img
           class="absolute"
           src="/images/tech/mysql-logo.png"
           width="60"
           alt=""
-        />
+          @mouseenter="togglePause()"
+          @mouseleave="togglePause()"
+        >
         <img
           class="absolute left-15"
           src="/images/tech/postgres-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-10 top-10"
           src="/images/tech/mongo-logo.png"
           width="60"
           alt=""
-        />
+        >
       </motion.div>
 
-      <motion.div class="relative left-8"
-      @mouseenter="togglePause()" @mouseleave="togglePause()"
-      :style="pos[3]">
+      <motion.div
+        class="relative left-8"
+        :style="pos[3]" @mouseenter="togglePause()"
+        @mouseleave="togglePause()"
+      >
         <img
           class="absolute"
           src="/images/tech/docker-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-15"
           src="/images/tech/kubernetes-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-10 top-10"
           src="/images/tech/vmware-logo.png"
           width="60"
           alt=""
-        />
+        >
       </motion.div>
 
-      <motion.div class="relative left-8"
-      @mouseenter="togglePause()" @mouseleave="togglePause()"
-      :style="pos[4]">
+      <motion.div
+        class="relative left-8"
+        :style="pos[4]" @mouseenter="togglePause()"
+        @mouseleave="togglePause()"
+      >
         <img
           class="absolute"
           src="/images/tech/linux-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-12"
           src="/images/tech/cisco-pt-logo.png"
           width="60"
           alt=""
-        />
+        >
       </motion.div>
 
-      <motion.div class="relative left-8"
-      @mouseenter="togglePause()" @mouseleave="togglePause()"
-      :style="pos[5]">
+      <motion.div
+        class="relative left-8"
+        :style="pos[5]" @mouseenter="togglePause()"
+        @mouseleave="togglePause()"
+      >
         <img
           class="absolute left-4"
           src="/images/tech/go-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-18"
           src="/images/tech/c-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-2 top-12"
           src="/images/tech/unity-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-15 top-16"
           src="/images/tech/elixir-logo.png"
           width="60"
           alt=""
-        />
+        >
       </motion.div>
 
-      <motion.div class="relative left-8"
-      @mouseenter="togglePause()" @mouseleave="togglePause()"
-      :style="pos[6]">
+      <motion.div
+        class="relative left-8"
+        :style="pos[6]" @mouseenter="togglePause()"
+        @mouseleave="togglePause()"
+      >
         <img
           class="absolute left-4"
           src="/images/tech/java-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-18"
           src="/images/tech/spring-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-2 top-14"
           src="/images/tech/php-logo.png"
           width="60"
           alt=""
-        />
+        >
         <img
           class="absolute left-16 top-14"
           src="/images/tech/laravel-logo.png"
           width="60"
           alt=""
-        />
+        >
       </motion.div>
     </div>
   </section>
