9a2bddb8c26febe4416eff4096083fa239a05a9c
Vuth Menghuor <hourousa9@gmail.com>
2025-11-26 23:42:19 +0700

    Update Hero component and related files

--- app/components/app/Hero.vue
+++ app/components/app/Hero.vue
@@ -1,242 +1,380 @@
 <script setup>
 import { motion, useTime, useTransform } from "motion-v";
+import { useHeroRotation } from "~/composables/rotation-cordonate";
 
-const duration = 5; // 2 sec
-const distance = 180;
-const origin = 20;
+// Animation configuration
+const duration = 1; // Slow rotation for smooth effect
+const middleDuration = 2; // Slower for middle orbit
+const outerOrbitDuration = 3; // Slowest for outer orbit
 
-const multiplier = Array.from({ length: 7 }, (_, i) => i * 800);
+const t1 = duration / 5; // 5 icons in inner orbit
+const t2 = middleDuration / 8; // 8 icons in middle orbit
+const t3 = outerOrbitDuration / 9; // 9 icons in outer orbit
 
-const js = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[0] * duration,
+const innerOrbit1 = useHeroRotation({
+  origin: 0,
+  distance: 140,
+  timeOffset: t1 * 0,
   duration,
 });
-const py = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[1] * duration + 500,
+const innerOrbit2 = useHeroRotation({
+  origin: 0,
+  distance: 140,
+  timeOffset: t1 * 1,
   duration,
 });
-const db = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[2] * duration,
+const innerOrbit3 = useHeroRotation({
+  origin: 0,
+  distance: 140,
+  timeOffset: t1 * 2,
   duration,
 });
-const ops = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[3] * duration,
+const innerOrbit4 = useHeroRotation({
+  origin: 0,
+  distance: 140,
+  timeOffset: t1 * 3,
   duration,
 });
-const tux = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[4] * duration,
+const innerOrbit5 = useHeroRotation({
+  origin: 0,
+  distance: 140,
+  timeOffset: t1 * 4,
   duration,
 });
-const cool = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[5] * duration + 500,
-  duration,
+
+// Middle orbit (215px radius) - 8 icons
+const middleOrbit1 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 5,
+  duration: middleDuration,
 });
-const php = useHeroRotation({
-  origin,
-  distance,
-  timeOffset: multiplier[6] * duration + 1500,
-  duration,
+const middleOrbit2 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 6,
+  duration: middleDuration,
+});
+const middleOrbit3 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 7,
+  duration: middleDuration,
+});
+const middleOrbit4 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 8,
+  duration: middleDuration,
+});
+const middleOrbit5 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 9,
+  duration: middleDuration,
+});
+const middleOrbit6 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 10,
+  duration: middleDuration,
+});
+const middleOrbit7 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 11,
+  duration: middleDuration,
+});
+const middleOrbit8 = useHeroRotation({
+  origin: 0,
+  distance: 215,
+  timeOffset: t2 * 12,
+  duration: middleDuration,
+});
+
+// Outer orbit (300px radius) - 9 icons
+const outerOrbit1 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 13,
+  duration: outerOrbitDuration,
+});
+const outerOrbit2 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 14,
+  duration: outerOrbitDuration,
+});
+const outerOrbit3 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 15,
+  duration: outerOrbitDuration,
+});
+const outerOrbit4 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 16,
+  duration: outerOrbitDuration,
+});
+const outerOrbit5 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 17,
+  duration: outerOrbitDuration,
+});
+const outerOrbit6 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 18,
+  duration: outerOrbitDuration,
+});
+const outerOrbit7 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 19,
+  duration: outerOrbitDuration,
+});
+const outerOrbit8 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 20,
+  duration: outerOrbitDuration,
+});
+const outerOrbit9 = useHeroRotation({
+  origin: 0,
+  distance: 285,
+  timeOffset: t3 * 21,
+  duration: outerOrbitDuration,
 });
 </script>
 
 <template>
   <section
-    class="hero-section justify-center md:items-center lg:items-start md:justify-center lg:justify-around flex flex-row md:flex-col lg:flex-row"
+    class="hero-section flex items-center justify-center relative min-h-[700px]"
   >
-    <!-- <div class="text relative bottom-25">
-      <img
-        class="relative right-8 top-20"
-        src="/images/gic-logo-big.png"
-        width="600"
-        alt="gic-logo"
-      />
-      <h1 class="text-4xl">See what our students built!</h1>
-
-      <div class="flex flex-row space-x-4 text-2xl font-semibold items-center">
-        <div>
-          <p>250+</p>
-          <p>Projects</p>
-        </div>
-        <USeparator orientation="vertical" class="h-20" />
-        <div>
-          <p>250+</p>
-          <p>GICats</p>
-        </div>
-      </div>
-      <br />
-      <h2 class="text-2xl font">for students by students. ₍^. ̫.^₎</h2>
-    </div> -->
-
-    <div class="relative top-20 w-40 right-20 md:h-300 lg:h-200">
-      <div class="relative top-28 left-12">
-        <img src="/images/tech/blackhole.png" width="120" alt="" />
-      </div>
-
-      <motion.div class="relative left-10" :style="js">
-        <img
-          class="absolute"
-          src="/images/tech/javascript-logo.jpg"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-15"
-          src="/images/tech/vue-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-10 top-10"
-          src="/images/tech/react-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
-
-      <motion.div class="relative left-8" :style="py">
-        <img
-          class="absolute"
-          src="/images/tech/python-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-15"
-          src="/images/tech/tensorflow-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-10 top-10"
-          src="/images/tech/torch-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
-
-      <motion.div class="relative left-8" :style="db">
-        <img
-          class="absolute"
-          src="/images/tech/mysql-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-15"
-          src="/images/tech/postgres-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-10 top-10"
-          src="/images/tech/mongo-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
-
-      <motion.div class="relative left-8" :style="ops">
-        <img
-          class="absolute"
-          src="/images/tech/docker-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-15"
-          src="/images/tech/kubernetes-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-10 top-10"
-          src="/images/tech/vmware-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
-
-      <motion.div class="relative left-8" :style="tux">
-        <img
-          class="absolute"
-          src="/images/tech/linux-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-12"
-          src="/images/tech/cisco-pt-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
-
-      <motion.div class="relative left-8" :style="cool">
-        <img
-          class="absolute left-4"
-          src="/images/tech/go-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-18"
-          src="/images/tech/c-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-2 top-12"
-          src="/images/tech/unity-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-15 top-16"
-          src="/images/tech/elixir-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
-
-      <motion.div class="relative left-8" :style="php">
-        <img
-          class="absolute left-4"
-          src="/images/tech/java-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-18"
-          src="/images/tech/spring-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-2 top-14"
-          src="/images/tech/php-logo.png"
-          width="60"
-          alt=""
-        />
-        <img
-          class="absolute left-16 top-14"
-          src="/images/tech/laravel-logo.png"
-          width="60"
-          alt=""
-        />
-      </motion.div>
+    <!-- Central content -->
+    <div class="relative z-10 text-center">
+      <h1 class="text-4xl mb-2 font-bold text-slate-100">TechHub</h1>
+      <p class="text-xl text-slate-200">
+        Showcasing student <br />tech brilliance
+      </p>
     </div>
+
+    <!-- Decorative circles for visual orbit effect -->
+    <div
+      class="orbit-circle outer absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[570px] h-[570px] rounded-full border-2 border-gray-400/20"
+    ></div>
+    <div
+      class="orbit-circle middle absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[430px] h-[430px] rounded-full border-2 border-gray-400/30"
+    ></div>
+    <div
+      class="orbit-circle inner absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[280px] h-[280px] rounded-full border-2 border-gray-400/40"
+    ></div>
+
+    <!-- Inner orbit icons -->
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="innerOrbit1"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img
+          src="/images/tech/javascript-logo.jpg"
+          class="w-full h-full object-contain rounded-lg"
+        />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="{ ...innerOrbit2, zIndex: 3 }"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/vue-logo.png" alt="Vue" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="{ ...innerOrbit3, zIndex: 2 }"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/react-logo.png" alt="React" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="innerOrbit4"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/linux-logo.png" alt="Linux" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="innerOrbit5"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/cisco-pt-logo.png" alt="Cisco Packet Tracer" />
+      </div>
+    </motion.div>
+
+    <!-- Middle orbit icons -->
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="{ ...middleOrbit1, zIndex: 1 }"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/php-logo.png" alt="PHP" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit2"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/laravel-logo.png" alt="Laravel" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit3"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/java-logo.png" alt="Java" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit4"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/spring-logo.png" alt="Spring" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit5"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/go-logo.png" alt="Go" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit6"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/c-logo.png" alt="C" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit7"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/unity-logo.png" alt="Unity" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="middleOrbit8"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/elixir-logo.png" alt="Elixir" />
+      </div>
+    </motion.div>
+
+    <!-- Outer orbit icons -->
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit1"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/docker-logo.png" alt="Docker" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit2"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/kubernetes-logo.png" alt="Kubernetes" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit3"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/vmware-logo.png" alt="VMware" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit4"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/python-logo.png" alt="Python" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit5"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/tensorflow-logo.png" alt="TensorFlow" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit6"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/torch-logo.png" alt="PyTorch" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit7"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/mysql-logo.png" alt="MySQL" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit8"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/postgres-logo.png" alt="PostgreSQL" />
+      </div>
+    </motion.div>
+
+    <motion.div
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      :style="outerOrbit9"
+    >
+      <div class="w-16 h-16 rounded-2xl p-3 shadow-lg">
+        <img src="/images/tech/mongo-logo.png" alt="MongoDB" />
+      </div>
+    </motion.div>
   </section>
 </template>
 
@@ -268,5 +406,31 @@ const php = useHeroRotation({
 #react {
   top: 40px;
   left: 40px;
+}
+
+.orbit-circle {
+  transition: all 0.3s ease;
+}
+
+.orbit-circle.outer {
+  animation: glow 4s ease-in-out infinite;
+}
+
+.orbit-circle.middle {
+  animation: glow 3s ease-in-out infinite;
+}
+
+.orbit-circle.inner {
+  animation: glow 2s ease-in-out infinite;
+}
+
+@keyframes glow {
+  0%,
+  100% {
+    box-shadow: 0 0 20px rgba(156, 163, 175, 0.2);
+  }
+  50% {
+    box-shadow: 0 0 40px rgba(156, 163, 175, 0.4);
+  }
 }
 </style>
--- app/composables/rotation-cordonate.ts
+++ app/composables/rotation-cordonate.ts
@@ -1,22 +1,28 @@
-import { motion, useTime, useTransform } from 'motion-v'
+import { motion, useTime, useTransform } from "motion-v";
 
 interface HeroRotationParams {
-    origin: number
-    distance: number
-    timeOffset: number
-    duration: number
+  origin: number;
+  distance: number;
+  timeOffset: number;
+  duration: number;
 }
 
-export function useHeroRotation({ origin = 0, distance = 1, duration = 1, timeOffset = 0 }: HeroRotationParams) {
-    const time = useTime()
+export function useHeroRotation({
+  origin = 0,
+  distance = 1,
+  duration = 1,
+  timeOffset = 0,
+}: HeroRotationParams) {
+  const time = useTime();
 
-    const x = useTransform(() => Math.cos((time.get() + timeOffset) / 1000 / duration) * distance + origin)
-    const y = useTransform(() => Math.sin((time.get() + timeOffset) / 1000 / duration) * distance + origin)
+  const transform = useTransform(() => {
+    const angle = (time.get() + timeOffset * 12500) / 2000 / duration;
+    const x = Math.cos(angle) * distance + origin;
+    const y = Math.sin(angle) * distance + origin;
+    return `translate(${x}px, ${y}px)`;
+  });
 
-    const translationXY = {
-        x,
-        y,
-    }
-
-    return translationXY
+  return {
+    transform,
+  };
 }
